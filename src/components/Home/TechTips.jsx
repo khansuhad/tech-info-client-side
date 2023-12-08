336d3b1c1d339ca4a22d634d8603d648f310369d
suhad ahmod khan <suhadahmodkhan@gmail.com>
2023-12-09 01:23:13 +0600

    Update TechTips

--- src/components/Home/TechTips.jsx
+++ src/components/Home/TechTips.jsx
@@ -18,8 +18,8 @@ const {theme } = useContext(AuthContext)
                   In this section, we offer a variety of tech tips and tutorials to help you make the most of your devices and software. From smartphone tricks to troubleshooting common computer issues, our step-by-step guides will empower you to harness the full potential of your technology. Whether you are looking for advice on cybersecurity, app recommendations, or how to optimize your workflow, you will find it all here. Stay tuned for regular updates and empower yourself with tech know-how.
                 </p>
                     <div className="lg:flex gap-5 py-10 lg:py-0">
-                    <img src="https://i.ibb.co/wByn36j/download.jpg" alt="" className="w-96 h-96 rounded mx-auto" />
-                    <img src="https://i.ibb.co/NsdWZKQ/images.png" alt="" className="w-96 h-96 rounded mx-auto mt-4 lg:mt-0" />
+                    <img src="https://i.ibb.co/wByn36j/download.jpg" alt="" className="w-60 h-60 rounded mx-auto" />
+                    <img src="https://i.ibb.co/NsdWZKQ/images.png" alt="" className="w-60 h-60 rounded mx-auto mt-4 lg:mt-0" />
                     </div>
                 </div>
               
